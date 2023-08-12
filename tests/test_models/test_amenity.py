b85dc8aff5e4538aa1e532ca208233c4691f61bf
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-12 05:30:29 -0700

    fixing a requirement

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+testing for amenity class
+"""
+
+
+from models.amenity import Amenity
+import unittest
+
+
+class TestAmenity(unittest.TestCase):
+    """
+    testing for the class Amenity
+    """
+    Pass
