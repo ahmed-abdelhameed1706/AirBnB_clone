f789dbf75ec32642c13e1a3ad17beb75d96e802a
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-12 06:33:47 -0700

    another

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,13 +12,13 @@ class TestAmenity(unittest.TestCase):
     """
     testing for the class Amenity
     """
-    
-    def test_placehold(self):
-        """
-        placeholder for now
-        """
-        amen = Amenity()
-        self.assertEqual(type(amen), Amenity)
+    Pass
+    #def test_placehold(self):
+        #"""
+        #placeholder for now
+        #"""
+        #amen = Amenity()
+        #self.assertEqual(type(amen), Amenity)
 
 
 if __name__ == "__main__":
