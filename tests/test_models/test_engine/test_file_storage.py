8e591d7491d76a3c888d074d552b00ef8248aced
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-08-12 05:51:01 -0700

    test_file_storage.py

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+testing the file storage module
+"""
+
+
+from models import storage
+import unittest
+
+
+class TestStorage(unittest.TestCase):
+    """
+    this is the test class of the storage
+    """
+    Pass
