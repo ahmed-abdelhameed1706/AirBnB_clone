3dd10e0964fb4a2052e0e9df9f5149d62f11db62
Ahmed Abd El Hameed <ahmed.ahameed17@yahoo.com>
2023-08-18 20:51:51 +0300

    adding task 9

---
+++ web_static/styles/100-places.css
@@ -0,0 +1,160 @@
+.places {
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+}
+.places h1 {
+        font-size: 30px;
+        width: 100%;
+        padding-left: 1%;
+}
+
+.places article {
+        border: 1px solid #ff5a5f;
+        width: 390px;
+        padding: 20px;
+        margin: 20px;
+        text-align: center;
+        border-radius: 4px;
+        position: relative;
+        z-index: -10;
+}
+
+.places article h2 {
+        font-size: 30px;
+        margin-top: 0;
+}
+
+.places article .price_by_night {
+        color: #ff5a5f;
+        border: 4px solid #ff5a5f;
+        border-radius: 50%;
+        min-width: 60px;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        z-index: -10;
+        top: 2%;
+        right: 5%;
+}
+
+.places article .price_by_night p {
+        font-size: 30px;
+        display: inline-block;
+}
+
+.places article .information {
+        height: 80px;
+        border-top: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+        display: flex;
+        justify-content: space-evenly;
+        margin-top: 9%;
+}
+
+.places article .information .max_guest .icon {
+        background-image: url(../images/icon_group.png);
+}
+
+.places article .information .number_rooms .icon {
+        background-image: url(../images/icon_bed.png);
+}
+
+.places article .information .number_bathrooms .icon {
+        background-image: url(../images/icon_bath.png);
+}
+
+.places article .information .max_guest .icon,
+.places article .information .number_rooms .icon,
+.places article .information .number_bathrooms .icon {
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+        height: 42px;
+        margin-top: 20%;
+        margin-bottom: 0;
+}
+
+.places article .information .max_guest p,
+.places article .information .number_rooms p,
+.places article .information .number_bathrooms p {
+        margin-top: 0;
+}
+
+.places article .user,
+.places article .description,
+.places article .amenities,
+.places article .reviews {
+        text-align: left;
+}
+
+.places article .amenities,
+.places article .reviews {
+        margin-top: 10%;
+}
+
+.places article .amenities h2,
+.places article .reviews h2 {
+        font-size: 16px;
+        border-bottom: 1px solid #ddd;
+        padding-bottom: 2%;
+}
+.places article .amenities h3,
+.places article .reviews h3 {
+        font-size: 14px;
+}
+
+.places article .amenities p,
+.places article .reviews p {
+        font-size: 12px;
+}
+
+.places article .amenities ul,
+.places article .reviews ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+}
+
+.places article .amenities ul li {
+        margin-top: 5%;
+        display: flex;
+        align-items: center;
+}
+
+.places article .amenities ul li .tv {
+        background-image: url(../images/icon_tv.png);
+}
+
+.places article .amenities ul li .wifi {
+        background-image: url(../images/icon_wifi.png);
+}
+
+.places article .amenities ul li .pet {
+        background-image: url(../images/icon_pets.png);
+}
+
+.places article .amenities p {
+        display: inline-block;
+        margin: 0;
+        margin-left: 2%;
+        padding: 0;
+}
+.places article .amenities ul li span {
+        background-position: left;
+        background-size: contain;
+        background-repeat: no-repeat;
+        display: inline-block;
+        height: 22px;
+        width: 22px;
+}
+
+.places article .reviews h3 {
+        margin-bottom: 0;
+}
+
+.places article .reviews p {
+        margin-top: 1%;
+}
